465c14bfcee75c63685e64755b96127488a71c9b
rvprakash9596 <raviprakash9596@gmail.com>
2024-08-29 22:08:55 +0530

    Bharat Clock Components

---
+++ Projects/2-bharat-clock-version-one/src/components/ClockHeading.jsx
@@ -0,0 +1,5 @@
+let ClockHeading =()=>{
+return(<h1 className="fw-bolder p-2">Bharat Clock</h1>);
+    
+};
+export default ClockHeading;
---
+++ Projects/2-bharat-clock-version-one/src/components/ClockSlogan.jsx
@@ -0,0 +1,5 @@
+let ClockSlogan =()=>{
+    return(<h1 className="lead fw-bold">This is the clock that shows the time in Bharat at att times</h1>);
+        
+    };
+    export default ClockSlogan;
