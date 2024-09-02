f70a3056bb887898af9de14723b54cf5ddce2b5e
rvprakash9596 <raviprakash9596@gmail.com>
2024-09-02 21:55:40 +0530

    learning fragment

--- 18 Fragments/learning-fragment/src/App.jsx
+++ 18 Fragments/learning-fragment/src/App.jsx
@@ -1,5 +1,6 @@
 import FoodItems from "./components/FoodItems";
 import ErrorMessage from "./components/ErrorMessage";
+import Container from "./components/Container";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
@@ -9,12 +10,14 @@ function App() {
   let foodItems = ['Tea','Roti','Milk','Green Vegetables','Rice',"Ghee"];
 
   return (
-    <>
+
+      <Container>
       <h1 className="food-heading">Healthy Foods</h1>
       <ErrorMessage items={foodItems}>
       </ErrorMessage>
       <FoodItems items={foodItems}></FoodItems>
-    </>
+      </Container>
+      
   );
 }
 export default App;
--- 18 Fragments/learning-fragment/src/components/Item.jsx
+++ 18 Fragments/learning-fragment/src/components/Item.jsx
@@ -2,7 +2,7 @@ import styles from "./Item.module.css";
 
 const Item = ({ foodItem }) => {
   return (
-    <li className={`${styles["kg-item"]}`}>
+    <li className={`${styles["kg-item"]} list-group-item`}>
       <span className={styles["kg-span"]}>{foodItem}</span>
     </li>
   );
