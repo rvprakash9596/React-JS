255ccdf501338900866e7494d3beb1c90ed3b9cd
rvprakash9596 <raviprakash9596@gmail.com>
2024-09-04 23:17:44 +0530

    event handling in react

--- 18 Fragments/learning-fragment/src/components/FoodInput.jsx
+++ 18 Fragments/learning-fragment/src/components/FoodInput.jsx
@@ -6,7 +6,6 @@ const handleOnChange = (event) =>{
     console.log(event.target.value);
 }
 
-
 return <input type="text" placeholder="Enter Food Item Here" className={styles.foodInput}
 onChange={handleOnChange }
 />
